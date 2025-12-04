fbd29845561dfb268a064d4fdb297e9f12a50ff2
Aditya Dhyani <139427619+aditya1232121@users.noreply.github.com>
2025-12-04 17:09:29 +0530

    new commit

---
+++ force-app/main/default/classes/readme.txt
@@ -0,0 +1,15 @@
+go to external client app --> external client app manager ---> new external client app
+
+Name do  and Oauth on karo 
+
+callurl ---> authentication karne ke baad kidhr jaana hai // after token where u want to go 
+
+base url/services/oauth/success
+https://login.salesforce.com/services/oauth2/success
+
+
+oauth scope 
+
+Full access (full)
+Manage user data via APIs (api)
+Perform requests at any time (refresh_token
--- force-app/main/default/lwc/Form/Form.js
+++ force-app/main/default/lwc/Form/Form.js
@@ -1,7 +1,7 @@
 import { LightningElement, track } from "lwc";
-import getTowers from "@salesforce/apex/SiteController.getTowers";
-import getProject from "@salesforce/apex/SiteController.getProject";
-import getSites from "@salesforce/apex/SiteController.getSites";
+import getTowers from "@salesforce/apex/CustomController.getTowers";
+import getProject from "@salesforce/apex/CustomController.getProject";
+import getSiteVisits from "@salesforce/apex/CustomController.getSiteVisits";
 
 export default class Form extends LightningElement {
     @track selectYourProject = [];
@@ -10,12 +10,12 @@ export default class Form extends LightningElement {
     @track sites = [];
     @track error = null;
 
-    projectId = '';
-    towerId = '';
-    priceValue = '';
-    visitorName = '';
-    phone = '';
-    date = '';
+    projectId = "";
+    towerId = "";
+    priceValue = "";
+    visitorName = "";
+    phone = "";
+    date = "";
 
     connectedCallback() {
         this.loadProjects();
@@ -28,14 +28,20 @@ export default class Form extends LightningElement {
                 label: p.Name,
                 value: p.Id
             }));
+
             const prices = result
                 .map(p => p.Price_Range__c)
                 .filter(v => v)
                 .filter((v, i, a) => a.indexOf(v) === i);
-            this.selectOption = prices.map(v => ({ label: v, value: v }));
+
+            this.selectOption = prices.map(v => ({
+                label: v,
+                value: v
+            }));
+
             this.error = null;
         } catch {
-            this.error = 'Unable to load projects';
+            this.error = "Unable to load projects";
         }
     }
 
@@ -49,12 +55,12 @@ export default class Form extends LightningElement {
             const result = await getTowers({ projectId: this.projectId });
             this.selectYourTower = result.map(t => ({
                 label: t.Name,
-                value: t.Id
+                value: t.Name   // Use tower Name because Apex expects TowerName
             }));
             this.error = null;
         } catch {
             this.selectYourTower = [];
-            this.error = 'Unable to load towers';
+            this.error = "Unable to load towers";
         }
     }
 
@@ -66,20 +72,29 @@ export default class Form extends LightningElement {
         this.priceValue = e.detail.value;
     }
 
-    visitorNameChange(e) { this.visitorName = e.target.value; }
-    phoneChange(e) { this.phone = e.target.value; }
-    dateChange(e) { this.date = e.target.value; }
+    visitorNameChange(e) {
+        this.visitorName = e.target.value;
+    }
+
+    phoneChange(e) {
+        this.phone = e.target.value;
+    }
+
+    dateChange(e) {
+        this.date = e.target.value;
+    }
 
     async submitButtonHandler() {
         try {
-            const result = await getSites({
+            const result = await getSiteVisits({
                 projectId: this.projectId,
-                towerId: this.towerId
+                TowerName: this.towerId
             });
+
             this.sites = result;
             this.error = null;
         } catch {
-            this.error = 'Unable to fetch site visits';
+            this.error = "Unable to fetch site visits";
         }
     }
 }
