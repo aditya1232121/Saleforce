783f1aba44313936373f412ddbaf0767b3f7536f
Aditya Dhyani <139427619+aditya1232121@users.noreply.github.com>
2025-12-09 10:47:46 +0530

    new commit

---
+++ force-app/main/default/lwc/syncAccountButton/syncAccountButton.js
@@ -0,0 +1,29 @@
+import { LightningElement, api } from 'lwc';
+import syncAccountToAPI from '@salesforce/apex/AccountAPIService.syncAccountToAPI';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+export default class SyncAccountButton extends LightningElement {
+    @api recordId;
+
+    handleClick() {
+        syncAccountToAPI({ accountId: this.recordId })
+        .then(result => {
+            this.showToast('Success', 'API Called Successfully', 'success');
+            console.log('API Response:', result);
+        })
+        .catch(error => {
+            this.showToast('Error', error.body.message, 'error');
+            console.error(error);
+        });
+    }
+
+    showToast(title, message, variant) {
+        this.dispatchEvent(
+            new ShowToastEvent({
+                title,
+                message,
+                variant
+            })
+        );
+    }
+}
